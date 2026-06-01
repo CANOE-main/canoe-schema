f76917db9ee73b05a97269bab9c50ac579811289
Yamil <essusyamil@gmail.com>
2026-06-01 15:14:38 -0400

    fix

--- canoe_schema/__init__.py
+++ canoe_schema/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "3.2.0"
 
 from .base import CanoeBaseModel
-from .sql import get_sql_schema, get_sql_schema_path
+from .sql import get_sql_schema, schema_path
 
 __all__ = ["CanoeBaseModel", "__version__", "get_schema", "get_schema_path"]
